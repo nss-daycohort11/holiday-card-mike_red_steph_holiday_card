50e72f07208eeaeed45d5eeda35fb4c3c05da362
Mike Ulvila <mikeulvila@me.com>
2015-10-29 13:10:43 -0500

    initial push

---
+++ finnish.js
@@ -0,0 +1,15 @@
+var Translator = (function(translator) {
+	var lexicon = { 
+		"merry": "iloinen",  
+		"christmas": "joulu",
+		"and": "ja",
+		"happy": "onnellinen",
+		"new": "uusi",
+		"year": "vuosi"
+		};
+
+	translate.
+
+
+	console.log(lexicon.keys);
+})(Translator);
